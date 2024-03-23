0102c17a9f0ea21e9b113beedcb627d6c3e0daf1
vmhoang1703 <vmhoang1703@gmail.com>
2024-03-23 21:44:27 +0700

    add sidebar content, basic style for sidebar and add walless icon; but don't know how to import icon in @packages/ui in turborepo

--- packages/ui/src/sidebar.tsx
+++ packages/ui/src/sidebar.tsx
@@ -1,5 +1,12 @@
 import type { FC } from 'react';
 import {
+	Dashboard,
+	LibraryBooks,
+	PermMedia,
+	Settings,
+	Storage,
+} from '@mui/icons-material';
+import {
 	Box,
 	Divider,
 	List,
@@ -13,31 +20,110 @@ interface SidebarProps {
 	handleDrawerToggle: () => void;
 }
 
+interface SidebarItemProps {
+	href: string;
+	text: string;
+	icon: JSX.Element;
+}
+
+const SidebarItem: FC<SidebarItemProps> = ({ href, text, icon }) => (
+	<ListItem disablePadding sx={styles.listItem}>
+		<ListItemButton
+			component={Link}
+			href={href}
+			style={{ textDecoration: 'none' }}
+		>
+			{icon}
+			<Typography variant="body1" sx={styles.text}>
+				{text}
+			</Typography>
+		</ListItemButton>
+	</ListItem>
+);
+
 const Sidebar: FC<SidebarProps> = ({ handleDrawerToggle }) => {
 	return (
-		<Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-			<Typography variant="h6" sx={{ my: 2 }}>
-				<Link href="/" style={{ textDecoration: 'none' }}>
+		<Box onClick={handleDrawerToggle}>
+			<Typography variant="h6" sx={styles.title}>
+				<Link
+					href="/"
+					style={{
+						textDecoration: 'none',
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'center',
+					}}
+				>
 					MetaCMS
 				</Link>
 			</Typography>
 			<Divider />
-			<List>
-				{['Dashboard', 'Profile'].map((item) => (
-					<ListItem key={item} disablePadding>
-						<ListItemButton sx={{ textAlign: 'center' }}>
-							<Link
-								href={`/${item.toLowerCase()}`}
-								style={{ textDecoration: 'none' }}
-							>
-								{item}
-							</Link>
-						</ListItemButton>
-					</ListItem>
-				))}
+			<List sx={styles.list}>
+				<SidebarItem
+					href="/dashboard"
+					text="Dashboard"
+					icon={<Dashboard sx={styles.icon} />}
+				/>
+				<Typography variant="button" sx={styles.typography}>
+					Collection Types
+				</Typography>
+				<SidebarItem
+					href="/"
+					text="Content Manager"
+					icon={<Storage sx={styles.icon} />}
+				/>
+				<Typography variant="button" sx={styles.typography}>
+					Plugins
+				</Typography>
+				<SidebarItem
+					href="/"
+					text="Content-Type Builder"
+					icon={<LibraryBooks sx={styles.icon} />}
+				/>
+				<SidebarItem
+					href="/"
+					text="Media Library"
+					icon={<PermMedia sx={styles.icon} />}
+				/>
+				<Typography variant="button" sx={styles.typography}>
+					General
+				</Typography>
+				<SidebarItem
+					href="/"
+					text="Settings"
+					icon={<Settings sx={styles.icon} />}
+				/>
 			</List>
 		</Box>
 	);
 };
 
 export default Sidebar;
+
+const styles = {
+	title: {
+		my: 2,
+		textAlign: 'center',
+	},
+	icon: {
+		mr: 1,
+	},
+	list: {
+		padding: 0,
+	},
+	listItem: {
+		padding: '5px 0',
+	},
+	text: {
+		marginLeft: '8px',
+	},
+	typography: {
+		display: 'block',
+		gutterBottom: true,
+		mx: 2,
+		mt: 2.5,
+		fontSize: '0.875rem',
+		fontWeight: 500,
+		color: '#616161',
+	},
+};
