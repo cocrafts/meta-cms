2483337e067341a460daac627906d59f8d6652aa
vmhoang1703 <vmhoang1703@gmail.com>
2024-03-26 15:28:40 +0700

    promote 2 methods authentication with GG as an util method and then import these methods into component for handling authentication with GG

--- packages/ui/src/signInWithGoogleButton.tsx
+++ packages/ui/src/signInWithGoogleButton.tsx
@@ -4,51 +4,17 @@ import type { MouseEvent } from 'react';
 import { useState } from 'react';
 import GoogleIcon from '@mui/icons-material/Google';
 import { Avatar, Button, Menu, MenuItem, Tooltip } from '@mui/material';
-import { signOut } from 'firebase/auth';
-import firebase from 'firebase/compat/app';
 import { useSnapshot } from 'valtio';
 
-import auth from '../../../apps/web/app/utils/config';
+import {
+	handleSignInWithGG,
+	handleSignOut,
+} from '../../../apps/web/app/utils/authGG';
 import { useUserProfile } from '../../../apps/web/app/utils/hook/index';
 
 const SigninWithGoogleButton = () => {
 	const userProfile = useUserProfile();
 	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-
-	const handleSignInWithGoogle = async () => {
-		const provider = new firebase.auth.GoogleAuthProvider();
-		try {
-			const result = await auth.signInWithPopup(provider);
-			const user = result.user;
-			if (user) {
-				const firebaseUserIdToken = await user.getIdToken();
-				console.log(firebaseUserIdToken);
-				const response = await fetch('http://localhost:3005/api/signin', {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-						Authorization: `Bearer ${firebaseUserIdToken}`,
-					},
-					body: JSON.stringify({ token: firebaseUserIdToken }),
-				});
-				const data = await response.json();
-				console.log(data);
-			}
-		} catch (error) {
-			console.error(error);
-		}
-	};
-
-	const handleSignOut = () => {
-		signOut(auth)
-			.then(() => {
-				userProfile.isSignedIn = false;
-				userProfile.profile.photoURL = '';
-			})
-			.catch((error) => {
-				console.error(error);
-			});
-	};
 
 	const open = Boolean(anchorEl);
 	const handleMenuOpen = (event: MouseEvent<HTMLButtonElement>) => {
@@ -98,7 +64,7 @@ const SigninWithGoogleButton = () => {
 			) : (
 				<Button
 					color="inherit"
-					onClick={handleSignInWithGoogle}
+					onClick={handleSignInWithGG}
 					startIcon={<GoogleIcon />}
 				>
 					Sign in with GG
